309c53396638960118fd66ab6739e8d65cd4d24b
elkowar <5300871+elkowar@users.noreply.github.com>
2020-11-06 21:54:40 +0100

    Initial commit

---
+++ plover_retro_surround/__init__.py
@@ -0,0 +1,22 @@
+from plover import *
+from plover.formatting import _Context, _Action
+
+
+def __retro_surround(ctx: _Context, cmdline: str) -> _Action:
+    action: _Action = ctx.copy_last_action()
+    args = cmdline.split(":")
+    word_cnt = int(args[0])
+    surround_char = args[1]
+
+    last_words = "".join(ctx.last_fragments(count=word_cnt))
+
+    action.prev_replace = last_words
+    action.text = surround_char + last_words + surround_char
+    action.word = None
+    action.prev_attach = True
+
+    return action
+
+
+def retro_surround(*args, **kwargs) -> _Action:
+    return __retro_surround(*args, **kwargs)
